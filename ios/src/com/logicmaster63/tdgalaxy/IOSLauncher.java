ad45eda3dca25e1080867dd7bed86323b0e34c68
LogicMaster63 <justinmarentette11@gmail.com>
2018-07-10 13:17:04 -0400

    Added IOS project

---
+++ ios/src/com/logicmaster63/tdgalaxy/IOSLauncher.java
@@ -0,0 +1,22 @@
+package com.logicmaster63.tdgalaxy;
+
+import org.robovm.apple.foundation.NSAutoreleasePool;
+import org.robovm.apple.uikit.UIApplication;
+
+import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
+import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
+import com.logicmaster63.tdgalaxy.TDGalaxy;
+
+public class IOSLauncher extends IOSApplication.Delegate {
+    @Override
+    protected IOSApplication createApplication() {
+        IOSApplicationConfiguration config = new IOSApplicationConfiguration();
+        return new IOSApplication(new TDGalaxy(null, null, null, null, null, true), config);
+    }
+
+    public static void main(String[] argv) {
+        NSAutoreleasePool pool = new NSAutoreleasePool();
+        UIApplication.main(argv, null, IOSLauncher.class);
+        pool.close();
+    }
+}
